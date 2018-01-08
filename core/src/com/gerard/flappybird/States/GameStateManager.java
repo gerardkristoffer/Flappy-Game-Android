c09a47275eafe6c8ed4a467881f5b7687d42d94f
Gerard <gerardkristoffer.sabado@gmail.com>
2018-01-08 12:10:23 +0800

    First commit

---
+++ core/src/com/gerard/flappybird/States/GameStateManager.java
@@ -0,0 +1,39 @@
+package com.gerard.flappybird.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.Stack;
+
+/**
+ * Created by Gerard on 07/01/2018.
+ */
+
+public class GameStateManager {
+
+    private Stack<State> states;
+
+    public GameStateManager() {
+        states = new Stack<State>();
+    }
+
+    public void push(State state) {
+        states.push(state);
+    }
+
+    public void pop() {
+        states.pop();
+    }
+
+    public void set(State state) {
+        states.pop();
+        states.push(state);
+    }
+
+    public void update(float dt) {
+        states.peek().update(dt);
+    }
+
+    public void render(SpriteBatch sb) {
+        states.peek().render(sb);
+    }
+}
